cb8fac36dfcbf6169d9296af6f0a8a250cc87ddf
Andrej Golis <andrej@creedmedia.com>
2023-04-27 13:28:50 +0200

    ITINFRA-48 Make use of the Google login

--- default/google-callback.php
+++ default/google-callback.php
@@ -1,29 +1,22 @@
 <?php
-
 require 'header.php';
+require 'google-config.php';
+require 'GoogleLoginApi.php';
 
-$client_id = $_SERVER['CLIENT_ID'];
-$client_secret = $_SERVER['CLIENT_SECRET'];
-$client_redirect_url = $_SERVER['CLIENT_REDIRECT_URL'];
+$gapi = new GoogleLoginApi();
 
 if(isset($_GET['code'])) {
   try {
-//    $gapi = new GoogleLoginApi();
-
     // Get the access token
-//    $data = $gapi->GetAccessToken($client_id, $client_redirect_url, $client_secret, $_GET['code']);
+    $data = $gapi->GetAccessToken($client_id, $client_redirect_url, $client_secret, $_GET['code']);
 
-    // Access Tokem
-//    $access_token = $data['access_token'];
+    // Access Token
+    $access_token = $data['access_token'];
 
     // Get user information
-//    $user_info = $gapi->GetUserProfileInfo($access_token);
+    $user_info = $gapi->GetUserProfileInfo($access_token);
 
     // Now that the user is logged in you may want to start some session variables
-
-//    $_SESSION["fname"] = $user_info['given_name'];
-//    $_SESSION["lname"] = $user_info['family_name'];
-//    $_SESSION["email"] = $user_info['email'];
     $_SESSION["method"] = "Google";
 
     // You may now want to redirect the user to the home page of your website
---
+++ default/google-config.php
@@ -0,0 +1,4 @@
+<?php
+$client_id = $_SERVER['CLIENT_ID'];
+$client_secret = $_SERVER['CLIENT_SECRET'];
+$client_redirect_url = $_SERVER['CLIENT_REDIRECT_URL'];
